0120b740612e2e86200ca627f9b2a2d92dea279f
Cosmin Dumitru <c.dumitru@sig.eu>
2015-11-23 13:37:43 +0100

    move test code to correct package

---
+++ integrationtests/src/test/java/eu/foo/bar/FooClientIT.java
@@ -0,0 +1,25 @@
+package eu.foo.bar;
+
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+public class FooClientIT {
+
+    @Test
+    public void testFoo() throws Exception {
+
+        Client client = ClientBuilder.newClient();
+        final WebTarget target = client.target("http://localhost:8080").path("/foo/eu");
+
+        final String result = target.request(MediaType.TEXT_PLAIN).get(String.class);
+        assertThat(result.length(), not(0));
+
+    }
+}
